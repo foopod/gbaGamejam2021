ecd8ea83c21209d9b5f1c5090e52846eee35a584
Jono Shields <jonathonshields@gmail.com>
2021-04-26 11:50:03 +1200

    add classes

--- README.md
+++ README.md
@@ -9,6 +9,165 @@ So yeah, pull up a seat and enjoy the journey.
 + Refactor and make a player class
 + Improve collisions
 
+## Day 14-17
+
+### Major Refactor - Classes
+
+So. How do classes in C++ work? Yeah, that took some googling on its own. Actually just trying to figure out the weirdness of their constructors was time consuming.
+
+I ended up making a hitbox class that I could use for my character. This is what it looks like...
+
+``` cpp
+class Hitbox
+{
+    private:
+        int _x;
+        int _y;
+        int _width;
+        int _height;
+    public:
+        Hitbox(int x, int y, int width, int height)
+            : _x(x), _y(y), _width(width), _height(height)
+        {}
+        int x()
+        {
+            return _x;
+        }
+        int y()
+        {
+            return _y;
+        }
+        int width()
+        {
+            return _width;
+        }
+        int height()
+        {
+            return _height;
+        }
+
+};
+```
+
+This is clearly not something that you should be learning from me though. So I will leave a few links here..
+
+https://www.cplusplus.com/doc/tutorial/classes/
+
+Now comes what I have been dreading, a big refactor to create a player class.
+
+I am actually working in a few other changes at the same, just to make it even worse for me.
+
++ Add AABB collision based on hitboxes
++ Move towards a velocity based model for movement
++ Add state management that I can tie animations to further down the line.
+
+So lets try to go throught this together and hope it makes sense.
+
+### AABB Collisions
+
+AABB or Axis-Aligned Bounding Box is a simple way of detecting whether two rectangular objects are overlapping (assuming that neither is rotated - thus alighned). This works well for a lot of simple gameplay mechanices. And also why I introduced hitboxes on our player.
+
+Could I have just used the sprite size as the hitbox? Yes. But you can get much better precision by making your own, especially good if your character doesn't take up the whole 8x8 sprite size.
+
+![](readme-images/AABB.jpg)
+
+To do this I find the X component for the left and right sides and the Y component for both the top and bottom.
+
+``` cpp
+int l = pos.x().integer() - hitbox.width() / 2 + hitbox.x();
+int r = pos.x().integer() + hitbox.width() / 2 + hitbox.x();
+int u = pos.y().integer() - hitbox.height() / 2 + hitbox.y();
+int d = pos.y().integer() + hitbox.height() / 2 + hitbox.y();
+```
+
+> NOTE: The width and height are divided by two because the sprites pos is in its centre.
+
+Then I use those to determine what the player is colliding with at a point in time.
+
+So `get_map_cell(l, u, map)` will return the map cell that the top left corner is interacting with.
+
+We use this in a way where if any of the corners overlap with any of the 'floor' tiles, then it is considered a collision. If it collides the player is placed at the top of the tile and dy (vertical velocity) is set to 0.
+
+``` cpp
+if(get_map_cell(l, u, map) < 13) ||
+  get_map_cell(l, d, map) < 13 ||
+  get_map_cell(r, u, map) < 13 ||
+  get_map_cell(r, d, map) < 13){
+    return true;
+} else {
+    return false;
+}
+```
+
+And I made a fall() function that is applied every update...
+
+``` cpp
+void fall(bn::affine_bg_ptr map){
+    if(check_fall_collisions(_pos, _hitbox, map)){
+        if(_dy > 0)
+        {
+            _dy = 0; // set vertical velocity to 0
+            if(!_grounded){ // if we aren't already on the ground then lets move us to the top and set our flag
+                _pos.set_y(_pos.y() - modulo(_pos.y() + 4,8));
+                _grounded = true;
+            }
+        } else {
+            // character is jumping, lets not mess with this
+        }
+    } else { // otherwise we are falling and not grounded
+        _dy+= gravity; 
+        _grounded = false;
+    }
+}
+```
+
+Boom collisions done.
+
+![](readme-images/collisions.gif)
+
+If you noticed we also added a  `_grounded` flag to our player, this will later be tied to player animation states.
+
+### Acceleration and Velocity
+
+You might also notice that above we are now using dy to represent the delta of y (y velocity). This way we can do cool things like have the character accelerate from 0 to their max speed, rather than instantly be moving.
+
+Our `move_right()` function now flips the sprite and adds to the velocity rather than the position..
+
+``` cpp
+void move_right(){
+    _sprite.set_horizontal_flip(false);
+    _dx+= acc;
+}
+```
+
+Then we go to actually move the character we do a few different things...
+
+``` cpp
+// apply friction
+_dx = _dx * friction;
+_dy = _dy * friction;
+
+//apply limits
+if( _dx > max_dx){
+    _dx = max_dx;
+}
+if( _dy > max_dy){
+    _dy = max_dy;
+}
+
+// update position
+_pos.set_x(_pos.x() + _dx);
+_pos.set_y(_pos.y() + _dy);
+```
+
+As you can see we have a constant friction variable now that is applied to slow the character down if they aren't moving. We also clamp to a max speed in each direction. And lastly we apply our new dy and dx to the players current position.
+
+This is an exagerated example of this effect...
+
+![](readme-images/acceleration.gif)
+
+The code is kind of all over the place right now, but feel free to take a look at todays tag [day-17](https://github.com/foopod/gbaGamejam2021/releases/tag/day-17).
+
 ## Day 13
 
 Collisions.
@@ -48,7 +207,7 @@ The next function we make is one to check for collisions and stop the falling.
     void check_collisions(bn::sprite_ptr& sprite, bn::affine_bg_ptr& map)
     {
         // gets the current map cell the sprite is on.
-        bn::fixed current_cell = get_map_index(sprite.x(), sprite.y(), map.dimensions().width());
+        bn::fixed current_cell = get_map_cell(sprite.x(), sprite.y(), map.dimensions().width());
 
         // at the moment all I am doing is checking if the cell is not the sky cell (aka not zero)
         bool is_hit = map.map().cells_ref().value().at(current_cell.integer()) != 0;
@@ -149,7 +308,7 @@ So we can use `map_bg.map().cells_ref().value()` to get the cells and `at(x)` to
 So I made a helper method to get the cell number of an x and y co-ord. Which ends up being something like this..
 
 ```c++
-bn::fixed get_map_index(bn::fixed tile_x, bn::fixed tile_y, bn::fixed columns)
+bn::fixed get_map_cell(bn::fixed tile_x, bn::fixed tile_y, bn::fixed columns)
 {
     return (tile_y * columns) + tile_x;
 }
--- feline/src/main.cpp
+++ feline/src/main.cpp
@@ -27,31 +27,184 @@
 
 namespace
 {
+    //constants
+    constexpr const bn::fixed gravity = 0.2;
+    constexpr const bn::fixed jump_power = 5;
+    constexpr const bn::fixed acc = 0.2;
+    constexpr const bn::fixed max_dx = 2;
+    constexpr const bn::fixed max_dy = 2;
+    constexpr const bn::fixed friction = 0.9;
+
+     // text set up
+    bn::sprite_text_generator text_generator(variable_8x16_sprite_font);
+    //text_generator.set_center_alignment();
+    bn::vector<bn::sprite_ptr, 32> text_sprites;
+
+    class Hitbox
+
+    {
+        private:
+            int _x;
+            int _y;
+            int _width;
+            int _height;
+        public:
+            Hitbox(int x, int y, int width, int height)
+                : _x(x), _y(y), _width(width), _height(height)
+            {}
+            int x(){
+                return _x;
+            }
+            int y(){
+                return _y;
+            }
+            int width(){
+                return _width;
+            }
+            int height(){
+                return _height;
+            }
+
+    };
+
     bn::fixed modulo(bn::fixed a, bn::fixed m)
     {
-
         return a - m * ((a/m).right_shift_integer());
     }
 
-    bn::fixed get_map_index(bn::fixed x, bn::fixed y, bn::fixed map_size)
+    int get_map_cell(bn::fixed x, bn::fixed y, bn::affine_bg_ptr& map)
     {
-
-        return modulo((y.safe_division(8).right_shift_integer() * map_size/8 + x/8), map_size*8);
+        int map_size = map.dimensions().width();
+        int cell =  modulo((y.safe_division(8).right_shift_integer() * map_size/8 + x/8), map_size*8).integer();
+        return map.map().cells_ref().value().at(cell);
     }
 
-    bn::fixed_point apply_gravity(bn::fixed_point pos)
+    bool check_fall_collisions(bn::fixed_point pos, Hitbox hitbox, bn::affine_bg_ptr& map)
     {
-        return bn::fixed_point(pos.x(), pos.y() +1);
-    }
+        int l = pos.x().integer() - hitbox.width() / 2 + hitbox.x();
+        int r = pos.x().integer() + hitbox.width() / 2 + hitbox.x();
+        int u = pos.y().integer() - hitbox.height() / 2 + hitbox.y();
+        int d = pos.y().integer() + hitbox.height() / 2 + hitbox.y();
 
-    void check_collisions(bn::sprite_ptr& sprite, bn::affine_bg_ptr& map)
-    {
-        bn::fixed current_cell = get_map_index(sprite.x(), sprite.y(), map.dimensions().width());
-        bool is_hit = map.map().cells_ref().value().at(current_cell.integer()) != 0;
-        if(is_hit){
-            sprite.set_y(sprite.y() -1);
+        //display current tile
+        text_sprites.clear();
+        text_generator.generate(0, -40, 
+        bn::to_string<32>(get_map_cell(l, u, map)) + " " +
+        bn::to_string<32>(get_map_cell(l, d, map)) + " " +
+        bn::to_string<32>(get_map_cell(r, u, map)) + " " +
+        bn::to_string<32>(get_map_cell(r, d, map)), text_sprites);
+
+        if((get_map_cell(l, u, map) > 0 && get_map_cell(l, u, map) < 13) ||
+        (get_map_cell(l, d, map) > 0 && get_map_cell(l, d, map) < 13) ||
+        (get_map_cell(r, u, map) > 0 && get_map_cell(r, u, map) < 13) ||
+        (get_map_cell(r, d, map) > 0 && get_map_cell(r, d, map) < 13)){
+            return true;
+        } else {
+            return false;
         }
     }
+    
+    class Player
+    {
+        public:
+            Player(bn::fixed_point pos, bn::sprite_ptr sprite, bn::camera_ptr camera)
+            : _pos(pos), _sprite(sprite), _camera(camera), _hitbox(0,3,4,2)
+            {
+                
+            }
+
+            bn::fixed_point pos(){
+                return _pos;
+            }
+
+            Hitbox hitbox(){
+                return _hitbox;
+            }
+
+            void jump(){
+                _dy-= jump_power;
+            }
+
+            void fall(bn::affine_bg_ptr map){
+                if(check_fall_collisions(_pos, _hitbox, map)){
+                    if(_dy > 0)
+                    {
+                        _dy = 0;
+                        if(!_grounded){
+                            _pos.set_y(_pos.y() - modulo(_pos.y() + 4,8));
+                            _grounded = true;
+                        }
+                    }
+                } else {
+                    _dy+= gravity;
+                    _grounded = false;
+                }
+                
+            }
+
+            void move_right(){
+                _sprite.set_horizontal_flip(false);
+                _dx+= acc;
+            }
+
+            void move_left(){
+                _sprite.set_horizontal_flip(true);
+                _dx-= acc;
+            }
+
+            void update_position(bn::affine_bg_ptr map){
+                // take input
+                if(bn::keypad::left_held())
+                {
+                    move_left();
+                } 
+                else if(bn::keypad::right_held())
+                {
+                    move_right();
+                }
+
+                // fall (and eventually check falling colliders)
+                fall(map);
+                
+                // jump
+                if(bn::keypad::up_pressed())
+                {
+                    jump();
+                } 
+
+                // apply friction
+                _dx = _dx * friction;
+                _dy = _dy * friction;
+
+                //apply limits
+                if( _dx > max_dx){
+                    _dx = max_dx;
+                }
+                if( _dy > max_dy){
+                    _dy = max_dy;
+                }
+                
+                // update position
+                _pos.set_x(_pos.x() + _dx);
+                _pos.set_y(_pos.y() + _dy);
+
+                // update sprite
+                _sprite.set_x(_pos.x());
+                _sprite.set_y(_pos.y());
+
+                // update camera
+                _camera.set_x(_pos.x());
+            }
+
+        private: 
+            bn::fixed _dx;
+            bn::fixed _dy;
+            bn::fixed_point _pos;
+            bn::sprite_ptr _sprite;
+            bn::camera_ptr _camera;
+            bool _grounded;
+            Hitbox _hitbox;
+    };
 }
 
 int main()
@@ -59,7 +212,6 @@ int main()
     bn::core::init();
 
     bn::fixed_point init_pos = bn::fixed_point(60, 432);
-
 
     // current cell player is on
     bn::fixed current_cell = 0;
@@ -74,18 +226,20 @@ int main()
 
     bn::camera_ptr camera = bn::camera_ptr::create(init_pos.x(), init_pos.y());
 
-    // text set up
-    bn::sprite_text_generator text_generator(variable_8x16_sprite_font);
-    text_generator.set_center_alignment();
-    bn::vector<bn::sprite_ptr, 32> text_sprites;
+    cat_sprite.set_camera(camera);
+    map_bg.set_camera(camera);
+    Player player = Player(init_pos, cat_sprite, camera);
+
+   
     bn::music_items::song.play(0.5);
     
     while(true)
     {
-        cat_sprite.set_position(apply_gravity(cat_sprite.position()));
-        check_collisions(cat_sprite, map_bg);
+        //cat_sprite.set_position(apply_gravity(cat_sprite.position()));
+        //calc_position()
+        //check_collisions(cat_sprite, map_bg);
 
-    
+    /**
         if(bn::keypad::left_held())
         {
             cat_sprite.set_horizontal_flip(true);
@@ -110,14 +264,11 @@ int main()
             cat_sprite.set_y(cat_sprite.y() + 1);
             //camera.set_y(camera.y() + 1);
         }
-
-        //display current tile
-        current_cell = get_map_index(cat_sprite.x(), cat_sprite.y(), 512);
-        text_sprites.clear();
-        text_generator.generate(0, -40, bn::to_string<32>(map_bg.map().cells_ref().value().at(current_cell.integer())), text_sprites);
-
-        cat_sprite.set_camera(camera);
-        map_bg.set_camera(camera);
+*/
+        player.update_position(map_bg);
+        
+        //cat_sprite.set_camera(camera);
+        //map_bg.set_camera(camera);
         
         action.update();
         bn::core::update();
